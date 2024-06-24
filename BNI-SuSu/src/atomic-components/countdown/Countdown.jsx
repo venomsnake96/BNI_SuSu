bc4da486219859a4081b7c8371a390bf8eb3bf45
VenomSnake <claudiojaimez.sec3@gmail.com>
2024-06-24 15:13:28 -0600

    fix error in the timer

--- BNI-SuSu/src/atomic-components/countdown/Countdown.jsx
+++ BNI-SuSu/src/atomic-components/countdown/Countdown.jsx
@@ -21,7 +21,7 @@ const Countdowns = () => {
         (distance % (1000 * 60 * 60  * 24)) / (1000 * 60 * 60)
       );
       const minutes = Math.floor(
-        (distance % (1000 * 60 * 60 )) / (100 * 60)
+        (distance % (1000 * 60 * 60 )) / (1000 * 60)
       );
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
